5989a085e5da521c0659d656e63b1055cde4ff23
Tion <tion.jamesdominic@gmail.com>
2025-04-08 07:35:38 +0800

    finished the Merge function

--- Research.cpp
+++ Research.cpp
@@ -5,37 +5,56 @@ using namespace std;
 
 /*
 TODO 
-- Add Binary Search for early termination
-- Add Parallel computing for the merge steps
-- Finish the merge sort function
-- Add a file reading system to calculate data
-- Add output file as txt after finish sorting
-- Add timer to benchmark code
+- Add Binary Search for early termination | High Priority
+- Add Parallel computing for the merge steps | Medium Priority
+- Finish the merge sort function *Done*
+- Add a file reading system to calculate data | Low Priority
+- Add output file as txt after finish sorting | Medium Priority
+- Add timer to benchmark code | Low Priority
 */
 
-void merge(vector<int>& arr){
-	int size = arr.size();
+vector<int> merge(vector<int> Left, vector<int> Right){
+	vector<int> result;
+	int sizeL = Left.size();
+	int sizeR = Right.size();
+	int i = 0, j = 0;
 
-	for(int i=0; i < size; i++){
-		cout << arr[i] << " ";
+	while(i < sizeL && j < sizeR){
+		if (Left[i] <= Right[j])
+            result.push_back(Left[i++]);
+        else
+            result.push_back(Right[j++]);
+
 	}
-	cout << "\n";
-}
 
-void mergesort(vector<int>& arr)
+	while (i < sizeL)
+        result.push_back(Left[i++]);
+    while (j < sizeR)
+        result.push_back(Right[j++]);
+
+
+	return result;
+}
+vector<int> mergesort(vector<int>& arr)
 {
 	
 	
 	int size = arr.size();
+
+	if(size <= 1){
+		return arr;
+	}
+	
 	int mid = size / 2;
 	
-	cout << "Left half:\n";
 	vector<int> leftHalf(arr.begin(), arr.begin() + mid);
-	merge(leftHalf);
-	cout << "Rigt half:\n";
-    vector<int> rightHalf(arr.begin() + mid, arr.end());
-	merge(rightHalf);
-	
+	vector<int> rightHalf(arr.begin() + mid, arr.end());
+
+	leftHalf = mergesort(leftHalf);
+	rightHalf = mergesort(rightHalf); 
+
+
+	return merge(leftHalf, rightHalf);
 }
 
 
@@ -44,7 +63,11 @@ int main(){
 	
 	vector<int> Numbers = {41, 52, 6, 43, 49, 59, 38, 71, 34, 76, 40, 13, 67, 24, 54, 50, 65, 25, 85, 19};
 	
-	mergesort(Numbers);
+	Numbers = mergesort(Numbers);
+
+	for(int num: Numbers){
+		cout << num << " ";
+	}
 	
 	return 0;
 }
